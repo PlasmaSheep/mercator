12287b3cadc1b46c9219159a1c4217b6ce106195
PlasmaSheep <plasmasheep@gmail.com>
2014-07-20 14:45:38 -0700

    Laying out directory structure, added scripts to run tests and app

---
+++ runserver.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python
+
+"""Run the flask server.
+"""
+
+from app import app
+
+if __name__ == '__main__':
+    app.run()
+
---
+++ runtests.py
@@ -0,0 +1,9 @@
+from nose.core import main
+
+def run_tests():
+    print("foo")
+    main()
+
+if __name__ == "__main__":
+    run_tests();
+
